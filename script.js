80de4dfae00d8763ce10c844d29d27c89d8e1628
fperez-glo <fperez@globaltechsa.com.ar>
2021-05-11 20:21:35 -0300

    clase 2

---
+++ script.js
@@ -0,0 +1,20 @@
+
+nombre = prompt('Por favor indique su nombre');
+while (!nombre.trim()){
+    nombre=prompt('No puede saltar este paso. Por favor indique su nombre.')
+};
+
+edad = parseInt(prompt('Indique su edad'));
+while (isNaN(edad) ||  edad == '')//(!edad.isInteger() && edad<=0) 
+{
+    edad = parseInt(prompt('Por favor indique una edad valida.'))
+};
+
+if (nombre && edad>=18) {
+    alert(`Su nombre es ${nombre} y es mayor de edad.`)
+} else {
+    alert(`Su nombre es ${nombre} y es menor de edad.`)
+};
+
+
+
